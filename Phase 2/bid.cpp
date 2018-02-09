a523a25c2b58384abfed0cfb14d157f6697c049d
adwansyed <adwan.syed@uoit.net>
2018-02-09 17:07:32 -0500

    Refactoring and comments

--- Phase 2/bid.cpp
+++ Phase 2/bid.cpp
@@ -4,37 +4,65 @@
 
 
 float maximumBid = 999999.0f;
-// Constructors
+
+/**
+ * Constructors
+ */
 Bid::Bid() {
-  currentBid;
-  newBid;
-  minimumBid;
+        currentBid;
+        newBid;
+        minimumBid;
 }
 
 
 Bid::Bid(float cB, float nB, float mnB){
-  currentBid = cB;
-  newBid = nB;
-  minimumBid = mnB;
+        currentBid = cB;
+        newBid = nB;
+        minimumBid = mnB;
 }
 
+/**
+ * Desctructor
+ */
 Bid::~Bid() {
-  cout << "destroying item object!" << endl;
+        cout << "destroying item object!" << endl;
 }
 
-// getters
-float Bid::getCurrentBid(){return currentBid;}
-float Bid::getNewBid(){return newBid;}
-float Bid::getMinimumBid(){return minimumBid;}
+/**
+ * Accessors
+ * @return currentBid
+ * @return newBid
+ * @return minimumBid
+ */
+float Bid::getCurrentBid(){
+        return currentBid;
+}
+float Bid::getNewBid(){
+        return newBid;
+}
+float Bid::getMinimumBid(){
+        return minimumBid;
+}
 
-// setters
-void Bid::setCurrentBid(float cB) {currentBid = cB;}
-void Bid::setNewBid(float nB) {newBid = nB;}
-void Bid::setMinimumBid(float mnB) {minimumBid = mnB;}
+/**
+ * Mutators
+ * @param cB  What the current bid is
+ * @param nB  The value of a new bid the user submits
+ * @param mnB The value of the new minimum bid calculated after each bid
+ */
+void Bid::setCurrentBid(float cB) {
+        currentBid = cB;
+}
+void Bid::setNewBid(float nB) {
+        newBid = nB;
+}
+void Bid::setMinimumBid(float mnB) {
+        minimumBid = mnB;
+}
 
-// print Bid info testing
+// print Bid information primarily used for debugging
 void Bid::print() {
-  cout << "Current Bid: " + to_string(getCurrentBid()) << endl;
-  cout << "New Bid: " + to_string(getNewBid()) << endl;
-  cout << "minimumBid: " + to_string(getMinimumBid()) << endl;
+        cout << "Current Bid: " + to_string(getCurrentBid()) << endl;
+        cout << "New Bid: " + to_string(getNewBid()) << endl;
+        cout << "minimumBid: " + to_string(getMinimumBid()) << endl;
 }
